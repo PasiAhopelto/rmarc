0d25846ccc0309cf612647f78d769a35974ae7fd
Pasi Ahopelto <pahopelt@gmail.com>
2015-03-30 22:29:34 +0300

    Corrupted data causes error (stop)
    Added test for corrupted data and test data files for valid record,
    and valid records. Also included data structure design.

--- R/read.marc.R
+++ R/read.marc.R
@@ -1,8 +1,11 @@
 read.marc <- function(file) {
     input <- file(file, 'rb')
     data <- readBin(input, 'raw', n=1)
+    close(input)
     if(length(data) == 0) {
         warning(paste("Input file", file, "is empty"))
     }
-    close(input)   
+    else {
+        stop(paste("Input file", file, " contains corrupted data")) 
+    }
 }
--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -8,8 +8,28 @@ test_that("empty file causes warning", {
     expect_warning(read.marc(file = "../data/empty.dat"))
 })
 
-# Fails on broken record
+test_that("corrupted input file causes error", {
+    expect_error(read.marc(file = "../data/corrupted.dat"))
+})
 
-# Reads single record
+# 1b data frame with absolute sequence number
+#
+# REC#  FIELD   FIELD#      I1  I2      SUB     VALUE
+# 1     650       8         _   0       a       Shipbuilding
+# 1     650       8         _   0       z       Netherlands
+# 1     650       8         _   0       x       History.
+# 1     650       9         _   0       a       Navigation
+# 1     650       9         _   0       z       Europe
+# 1     650       9         _   0       x       History.
 
-# Reads multiple records
+# test_that("reads single record", {
+#     expect_error(expect_warning(read.marc(file = "../data/corrupted.dat")))
+#     ADD DATA FIXTURE
+#     CHECK THAT READ DATA EQUALS FIXTURE
+# )}
+
+# test_that("reads two records", {
+#     expect_error(expect_warning(read.marc(file = "../data/corrupted.dat")))
+#     ADD DATA FIXTURE
+#     CHECK THAT READ DATA EQUALS FIXTURE
+# )}
